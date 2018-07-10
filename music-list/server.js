cac4070ebe04137c74fc72b613c23f9a4b7c8f42
xiaoxiong <286536753@qq.com>
2018-07-10 23:51:59 +0800

    修改了播放器采用express

--- music-list/server.js
+++ music-list/server.js
@@ -1,21 +1,49 @@
 const express = require('express')
 const path = require('path')
 const xtpl = require('xtpl')
+const querystring = require('querystring')
+const fs = require('fs')
 // 创建app
 const app = express()
 //浏览器请求静态资源
 app.use(express.static(path.join(__dirname,'statics')))
 
+// 文件中存在文字或其他特殊字符
+
+
 
 app.get('/list.html',(req,res)=>{
     xtpl.renderFile(path.join(__dirname,'list.html'),{
-        musics:path.join(__dirname,'musics.json')
+        // 获取json数据路径，并把它读出来，切记require
+        musics:require(path.join(__dirname,'musics.json'))
     },(err,content)=>{
+        if(err){
+            console.log(err)
+        }
        res.send(content)
     })
 })
 
-// app.get()
+app.get('/site.css',(req,res)=>{
+    res.status(200).json({message:'样式添加成功'})
+})
+
+
+app.get('/juqery.min.js',(req,res)=>{
+    res.status(200).json({message:'样式添加成功'})
+})
+
+// querystring.unescape()
+app.get('/musics',(req,res)=>{
+    // console.log(req)
+    const url = querystring.unescape(req.url)
+    console.log(url)
+    // fs.readFile(path.join(__dirname,url),(err,data)=>{
+    //     res.setHeader("Content-Type","audio/mpeg;charset=utf-8")
+    //     res.send(data)
+    // })
+    res.status(200).json({message:'样式添加成功'})
+})
 
 app.listen(80, '127.0.0.1', err => {
     if (err) {
